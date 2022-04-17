83811edcada8c06444a425dc5f69f3efec7aa8d3
thanhnhile <marrythanhnhipl205@gmail.com>
2022-04-18 00:14:45 +0700

    book layout - book adapter

---
+++ app/src/main/java/com/example/book_store/BookAdpater.java
@@ -0,0 +1,57 @@
+package com.example.book_store;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class BookAdpater extends BaseAdapter {
+    private Context context;
+    LayoutInflater inflater = null;
+    ArrayList<Book> list;
+    {
+        list = new ArrayList<>();
+    }
+
+    public BookAdpater(Context context,ArrayList<Book> list) {
+        this.context = context;
+        this.inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        this.list = list;
+    }
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return list.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        if(view == null){
+            view = inflater.inflate(R.layout.book_layout, viewGroup, false);
+        }
+        Book book = list.get(i);
+        ImageView img = view.findViewById(R.id.book_img);
+        TextView title = view.findViewById(R.id.book_title);
+        TextView price = view.findViewById(R.id.book_price);
+        img.setImageResource(book.getImg());
+        title.setText(book.getTitle());
+        String bookPrice = Integer.toString(book.getPrice());
+        price.setText(bookPrice);
+        return view;
+    }
+}

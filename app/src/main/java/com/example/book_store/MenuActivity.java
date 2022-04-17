682541778222957cd675b4bfea108c260e92f4e9
thanhnhile <marrythanhnhipl205@gmail.com>
2022-04-17 17:26:17 +0700

    navigation

---
+++ app/src/main/java/com/example/book_store/MenuActivity.java
@@ -0,0 +1,46 @@
+package com.example.book_store;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MenuActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+    HomeFragment homeFragment = new HomeFragment();
+    SearchFragment searchFragment = new SearchFragment();
+    CartFragment cartFragment = new CartFragment();
+    AccountFragment accountFragment = new AccountFragment();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.menu_nav);
+        getSupportFragmentManager().beginTransaction().replace(R.id.container,homeFragment).commit();
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.home:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,homeFragment).commit();
+                        return true;
+                    case R.id.search:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,searchFragment).commit();
+                        return true;
+                    case R.id.cart:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,cartFragment).commit();
+                        return true;
+                    case R.id.account:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,accountFragment).commit();
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+}

28c4e7297f1e1a0d5aec6ac39a5776a4e3fe7670
thanhnhile <marrythanhnhipl205@gmail.com>
2022-04-21 22:25:43 +0700

    config firebase

--- app/src/main/java/com/example/book_store/LoginActivity.java
+++ app/src/main/java/com/example/book_store/LoginActivity.java
@@ -4,11 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
+public class LoginActivity extends AppCompatActivity {
+    FirebaseDatabase database;
+    DatabaseReference myRef;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference("hello");
+        myRef.setValue("Hello world");
     }
 }

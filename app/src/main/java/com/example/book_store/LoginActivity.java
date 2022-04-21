c2748d2180dbbebb0205ff0f37cf13c0ec6fa204
thanhnhile <marrythanhnhipl205@gmail.com>
2022-04-21 23:45:24 +0700

    login, register user

--- app/src/main/java/com/example/book_store/LoginActivity.java
+++ app/src/main/java/com/example/book_store/LoginActivity.java
@@ -1,21 +1,82 @@
 package com.example.book_store;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.example.book_store.model.User;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class LoginActivity extends AppCompatActivity {
-    FirebaseDatabase database;
-    DatabaseReference myRef;
+    private FirebaseDatabase database;
+    private DatabaseReference myRef;
+    EditText txtPhone,txtPass;
+    Button btnLogin, btnRegister;
+    private User user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         database = FirebaseDatabase.getInstance();
-        myRef = database.getReference("hello");
-        myRef.setValue("Hello world");
+        myRef = database.getReference("Users");
+        txtPhone = (EditText) findViewById(R.id.login_txtPhone);
+        txtPass = (EditText) findViewById(R.id.login_txtPass);
+        btnLogin = findViewById(R.id.login_btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String phone = txtPhone.getText().toString().trim();
+                String pass = txtPass.getText().toString().trim();
+                if(phone.isEmpty() || pass.length() < 6){
+                    Toast.makeText(LoginActivity.this, "Trường không được để trống", Toast.LENGTH_SHORT).show();
+                }else{
+                    myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            if(snapshot.hasChild(phone)){
+                                final User user = snapshot.child(phone).getValue(User.class);
+                                if(user.getPassword().equals(pass)){
+                                    Toast.makeText(LoginActivity.this, "Đăng nhập thành công", Toast.LENGTH_SHORT).show();
+                                    //Main menu Activity
+                                    //Gui user
+                                }
+                                else{
+                                    Toast.makeText(LoginActivity.this, "Đăng nhập thất bại!\nVui lòng thử lại", Toast.LENGTH_LONG).show();
+                                }
+
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+                }
+
+            }
+        });
+        btnRegister = (Button) findViewById(R.id.login_btnRegister);
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                changeToRegisterActivity();
+            }
+        });
+
+    }
+    private void changeToRegisterActivity(){
+        Intent intent = new Intent(getApplicationContext(),SignupActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/book_store/SignupActivity.java
+++ app/src/main/java/com/example/book_store/SignupActivity.java
@@ -1,14 +1,98 @@
 package com.example.book_store;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.book_store.model.User;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class SignupActivity extends AppCompatActivity {
-
+    EditText txtFullName,txtPhone,txtPass;
+    Button btnRegister,btnLogin;
+    private FirebaseDatabase database;
+    private DatabaseReference myRef;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference("Users");
+        txtFullName = (EditText) findViewById(R.id.txtFullname);
+        txtPhone = (EditText) findViewById(R.id.txtPhone);
+        txtPass = (EditText) findViewById(R.id.txPass);
+        btnRegister = (Button)findViewById(R.id.btnRegister);
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                register();
+            }
+        });
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                changeToLoginActivity();
+            }
+        });
+    }
+    private void register(){
+        String phone = txtPhone.getText().toString();
+        String fullName = txtFullName.getText().toString();
+        String pass = txtPass.getText().toString();
+        if(isValid(phone,fullName,pass)){
+            User user = new User(fullName,phone,pass,"None");
+            myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    //Check phone number is exist
+                    if(snapshot.hasChild(phone)){
+                        Toast.makeText(SignupActivity.this, "Số điện thoại đã tồn tại", Toast.LENGTH_SHORT).show();
+                    }
+                    else{
+                        myRef.child(user.getPhone()).setValue(user);
+                        Toast.makeText(SignupActivity.this, "Đăng ký thành công", Toast.LENGTH_SHORT).show();
+                        //Login
+                        changeToLoginActivity();
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
+
+        }
+    }
+    private Boolean isValid(String phone, String fullName, String password){
+        String validNumber = "^[+]?[0-9]{8,15}$";
+        if(!phone.matches(validNumber)){
+            Toast.makeText(SignupActivity.this,"Số điện thoại không hợp lệ!",Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        if(fullName.trim().isEmpty() || password.isEmpty()){
+            Toast.makeText(SignupActivity.this,"Không được để trống",Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        if(password.length() < 6){
+            Toast.makeText(SignupActivity.this,"Vui lòng nhập mật khẩu dài hơn 6 ký tự!",Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
+    }
+    private void changeToLoginActivity(){
+        Intent loginIntent = new Intent(getApplicationContext(),LoginActivity.class);
+        startActivity(loginIntent);
     }
 }

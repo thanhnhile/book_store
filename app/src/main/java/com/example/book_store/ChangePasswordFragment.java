c8a78878f52cff6ade708552b30cf39549b845d3
Vy Tran <19110503@student.hcmute.edu.vn>
2022-05-19 00:51:16 +0700

    UpdateInforFragment

--- app/src/main/java/com/example/book_store/ChangePasswordFragment.java
+++ app/src/main/java/com/example/book_store/ChangePasswordFragment.java
@@ -4,61 +4,60 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ChangePasswordFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.example.book_store.sharedpreferences.Constants;
+import com.example.book_store.sharedpreferences.PreferenceManager;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthCredential;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
+
+
 public class ChangePasswordFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    EditText txtPass,txtNewPass;
+    Button btnSave;
+    Button btnBack;
+    private String phone;
 
     public ChangePasswordFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ChangePasswordFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ChangePasswordFragment newInstance(String param1, String param2) {
-        ChangePasswordFragment fragment = new ChangePasswordFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_change_password, container, false);
+        View view = inflater.inflate(R.layout.fragment_change_password, container, false);
+        txtPass = view.findViewById(R.id.changePass_pass);
+        txtNewPass = view.findViewById(R.id.changePass_newPass);
+        btnSave = view.findViewById(R.id.changePass_btnSave);
+//
+//        btnSave.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                String oldPass = txtPass.getText().toString().trim();
+//                String newPass = txtNewPass.getText().toString().trim();
+//                if (TextUtils.isEmpty(oldPass)) {
+//                    Toast.makeText(getActivity(), "Nhap mat khau hien tai", Toast.LENGTH_SHORT).show();
+//                }
+//                updatePassword(oldPass,newPass);
+//            }
+//        });
+        return view;
     }
+//    private void updatePassword(String oldPass,String newPass){
+//        PreferenceManager preferenceManager = new PreferenceManager(getContext(), Constants.LOGIN_KEY_PREFERENCE_NAME);
+//        phone = preferenceManager.getString(Constants.LOGIN_PHONE);
+//        final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+//        AuthCredential authCredential = getCredential(phone,oldPass)
+//    }
 }
--- app/src/main/java/com/example/book_store/UpdateInforFragment.java
+++ app/src/main/java/com/example/book_store/UpdateInforFragment.java
@@ -2,25 +2,122 @@ package com.example.book_store;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.book_store.model.User;
+import com.example.book_store.sharedpreferences.Constants;
+import com.example.book_store.sharedpreferences.PreferenceManager;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Transaction;
 
 public class UpdateInforFragment extends Fragment {
-    TextView txtName,txtPhone,txtAddress;
-
+    EditText txtName,txtPhone,txtAddress;
+    Button btnSave;
+    Button btnBack;
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference reference;
+    private String phone,name,address;
     public UpdateInforFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_update_infor, container, false);
+        View view =  inflater.inflate(R.layout.fragment_update_infor, container, false);
+        txtName = view.findViewById(R.id.updateinfor_txtname);
+        txtPhone = view.findViewById(R.id.updateinfor_txtphone);
+        txtAddress = view.findViewById(R.id.updateinfor_txtaddress);
+        btnSave = view.findViewById(R.id.updateinfor_btnUpdate);
+
+        reference = FirebaseDatabase.getInstance().getReference("Users");
+        showProfile();
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateInfor();
+            }
+        });
+        return view;
+    }
+
+    private void showProfile() {
+        PreferenceManager preferenceManager = new PreferenceManager(getContext(), Constants.LOGIN_KEY_PREFERENCE_NAME);
+        phone = preferenceManager.getString(Constants.LOGIN_PHONE);
+        reference = FirebaseDatabase.getInstance().getReference("Users");
+        reference.child(phone).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User profile = snapshot.getValue(User.class);
+
+                if (profile != null){
+                    name = profile.getFullName();
+                    address = profile.getAddress();
+
+                    txtName.setText(name);
+                    txtAddress.setText(address);
+                    txtPhone.setText(phone);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+    public void updateInfor() {
+        if (isNameChanged() || isAddressChanged()) {
+            Toast.makeText(getActivity(), "Da cap nhat thong tin", Toast.LENGTH_SHORT).show();
+        }
+        else Toast.makeText(getActivity(), "Khong the cap nhat thong tin", Toast.LENGTH_SHORT).show();
+    }
+    private boolean isNameChanged(){
+        PreferenceManager preferenceManager = new PreferenceManager(getContext(), Constants.LOGIN_KEY_PREFERENCE_NAME);
+        phone = preferenceManager.getString(Constants.LOGIN_PHONE);
+        if (! name.equals(txtName.getText().toString())) {
+            reference.child(phone).child("fullName").setValue(txtName.getText().toString());
+            name = txtName.getText().toString();
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    private boolean isAddressChanged(){
+        PreferenceManager preferenceManager = new PreferenceManager(getContext(), Constants.LOGIN_KEY_PREFERENCE_NAME);
+        phone = preferenceManager.getString(Constants.LOGIN_PHONE);
+        if (! address.equals(txtAddress.getText().toString())) {
+            reference.child(phone).child("address").setValue(txtAddress.getText().toString());
+            address = txtAddress.getText().toString();
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 }
